c6a437b5d5198b475fafaf35166dd046696dadc0
git-afsantos <contact.andre.santos@gmail.com>
2018-10-04 11:16:18 +0100

    updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name            = "bonsai-code",
-    version         = "0.3.1",
+    version         = "0.4.0",
     author          = "Andre Santos",
     author_email    = "andre.f.santos@inesctec.pt",
     description     = "Static analysis library.",
